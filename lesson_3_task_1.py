669173db3c44c10dad78e5532665045be221a47a
Alex120197 <146189396+Alex120197@users.noreply.github.com>
2025-08-10 01:15:31 +0400

    Add files via upload

---
+++ lesson_3_task_1.py
@@ -0,0 +1,5 @@
+from user import User
+my_user = User('Саша', 'Власов')
+print(my_user.first_name)
+print(my_user.last_name)
+print(my_user.first_name, my_user.last_name)
---
+++ lesson_3_task_2.py
@@ -0,0 +1,15 @@
+from smartphone import Smartphone # type: ignore
+phone1 = Smartphone
+phone2 = Smartphone
+phone3 = Smartphone
+phone4 = Smartphone
+phone5 = Smartphone
+phone1 = ("iPhone", "15 pro max", "+79271111111")
+phone2 = ("iphone", "11", "+79272222222")
+phone3 = ("Honor", "30", "+79273333333")
+phone4 = ("Samsung", "15", "+79274444444")
+phone5 = ("iPhone", "16 pro max", "+79275555555")
+
+catalog = [phone1, phone2, phone3, phone4, phone5]
+for phones in catalog:
+    print(phones)
