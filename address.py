0dd8df87dbe8f2047319cece47af5f1d6910aa39
Alex120197 <146189396+Alex120197@users.noreply.github.com>
2025-08-10 14:39:27 +0400

    Update address.py

--- address.py
+++ address.py
@@ -1,7 +1,8 @@
-class Address:
+class address:
     def __init__(self, index, city, street, home_number, apart_number):
         self.i = index
         self.c = city
         self.s = street
         self.h = home_number
+
         self.an = apart_number
