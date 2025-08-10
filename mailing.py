476d9957175ce1cd2275a9909b78da888262749b
Alex120197 <146189396+Alex120197@users.noreply.github.com>
2025-08-10 16:24:08 +0400

    Update mailing.py

--- mailing.py
+++ mailing.py
@@ -1,16 +1,6 @@
-from address import address
-class mailing:
-    to_address = address
-    from_address = address
-    cost = "1500"
-    track = "12345"
+class Mailing:
     def __init__(self, to_address, from_address, cost, track):
-        self.t = to_address
-        self.f = from_address
-        self.c = cost
-        self.t = track
-    def to_address(self):
-        print(self.to_address)
-    def from_address(self):
-
-        print(self.from_address)
+        self.to_address = to_address
+        self.from_address = from_address
+        self.cost = cost
+        self.track = track
