e0d7849f7226f7bc8a8177105ad4a71557c2ff31
Alex120197 <146189396+Alex120197@users.noreply.github.com>
2025-08-10 16:24:37 +0400

    Update lesson_3_task_3.py

--- lesson_3_task_3.py
+++ lesson_3_task_3.py
@@ -1,19 +1,10 @@
-from address import address
-from mailing import mailing
-to_address = address
-from_address = address
-to_address = 111111, "г. Самара", "ул. Ленина", 120, 7
-from_address = 222222, "г. Москва", "ул. Пушкина", 25, 17
-sending = Mailing (to_address, from_address, 1500, 12345)
-print(
-    "Отправление товара под номером",
-    sending.track,
-    "из",
-    from_address,
-    "в",
-    to_address,
-    ". Стоимость",
-    sending.cost,
-    "рублей.",
+from address import Address
+from mailing import Mailing
+to_address = Address("12345", "Москва", "Ленина", "10", "20")
+from_address = Address("54321", "Самара", "Пушкина", "120", "10")
+mailing = Mailing(to_address, from_address, 1500, "123")
 
-)
+print(f"Отправление товара под номером {mailing.track} из {mailing.from_address.index}, {mailing.from_address.city},"
+      f"{mailing.from_address.street}, {mailing.from_address.home_number} - {mailing.from_address.apart_number} "
+      f"в {mailing.to_address.index}, {mailing.to_address.city}, {mailing.to_address.street}, "
+      f"{mailing.to_address.home_number} - {mailing.to_address.apart_number}. Стоимость {mailing.cost} рублей.")
